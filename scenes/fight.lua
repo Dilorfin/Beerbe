e5e8a61af96d84e1596890c0f2b0e6f0f2484ef9
Dilorfin <pavel.kuybida@gmail.com>
2020-07-04 23:04:47 +0300

    Add action menu

--- scenes/fight.lua
+++ scenes/fight.lua
@@ -3,36 +3,81 @@ require "commands"
 
 local scene = {}
 
+local menuType = {
+    Action = 0,
+    Target = 1,
+    Magic = 2
+}
+
 function scene.load ()
-    print "scene loaded"
+    -- load resources
+    local prefix = "asserts/fight/"
+    --background = love.graphics.newImage(prefix.."background.jpg")
+    local iconsNames = {
+        prefix.."sword.png",
+        prefix.."shield.png",
+        prefix.."magic.png",
+        prefix.."beer-bottle.png",
+        prefix.."run.png"
+    }
+    icons = love.graphics.newArrayImage(iconsNames)
+
+    -- init variables
+    choose = 0
+    menu = menuType.Action
 end
 
 function scene.unload()
-    print "scene unloaded"
+    background = nil
+    icons = nil
 end
 
 function scene.update(delta_time)
-    print("scene update: "..delta_time)
+    
 end
 
 function scene.control_button(command)
-    if command == Command.Menu then
-        print "menu"
-    elseif command == Command.Confirm then
-        print "confirm"
-    elseif command == Command.Deny then
-        print "deny"
-    elseif command == Command.Up then
-        print "up"
+    if menu == menuType.Action then
+        if command == Command.Left then
+            if choose > 0 then
+                choose = choose - 1
+            end
+        elseif command == Command.Right then
+            if choose < icons:getLayerCount()-1 then
+                choose = choose + 1
+            end
+        elseif command == Command.Confirm then
+            print "confirm"
+        elseif command == Command.Deny then
+            print "deny"
+        end
     end
 end
 
 function scene.control_axis(x_axis, y_axis)
-    print(x_axis.." - "..y_axis)
+    
 end
 
 function scene.draw()
-    love.graphics.print("Hello world",0,0)
+    if menu == menuType.Action then
+        --love.graphics.draw(background, 0, 0, 0, love.graphics.getWidth() / background:getWidth(), love.graphics.getHeight() / background:getHeight())
+
+        local menuHeight = love.graphics.getHeight() / 6
+        local menuItemSize = love.graphics.getHeight() / 7
+        local offset = (menuHeight - menuItemSize)/2
+
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("fill", 0, love.graphics.getHeight() - menuHeight, love.graphics.getWidth(), menuHeight)
+        love.graphics.setColor(1, 1, 1)
+        
+        love.graphics.setLineWidth(2)
+        love.graphics.rectangle("line", offset + choose *  menuHeight, offset + love.graphics.getHeight() - menuHeight, menuItemSize, menuItemSize, 10, 10)
+        love.graphics.setLineWidth(1)
+
+        for i = 1, icons:getLayerCount() do
+            love.graphics.drawLayer(icons, i, offset + (i - 1) * menuHeight, offset + love.graphics.getHeight() - menuHeight, 0, menuItemSize / icons:getWidth())
+        end
+    end
 end
 
 return scene
