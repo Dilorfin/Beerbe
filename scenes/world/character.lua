ddd15856474c3c2402392d1a2d5eadc460972744
Dilorfin <pavel.kuybida@gmail.com>
2020-08-01 18:52:15 +0300

    Fix naming directions in world character

--- scenes/world/character.lua
+++ scenes/world/character.lua
@@ -3,8 +3,8 @@ require "animation"
 local character = require "character"
 character.animation = {
     states = {
-        forward = 1,
-        backward = 2,
+        upward = 1,
+        downward = 2,
         leftward = 3,
         rightward = 4
     }
@@ -12,8 +12,8 @@ character.animation = {
 
 function character.animation:load()
     local heroFilenames = {
-        "asserts/world/animations/forward.png",
-        "asserts/world/animations/backward.png",
+        "asserts/world/animations/upward.png",
+        "asserts/world/animations/downward.png",
         "asserts/world/animations/leftward.png",
         "asserts/world/animations/rightward.png"
     }
@@ -21,11 +21,18 @@ function character.animation:load()
     self.animation = newAnimation(heroSourceImage, 48, 48, 0.11, 3)
     self.animation:setMode("bounce")
 
-    self:setState("backward")
+    self:setState("downward")
 end
 
 function character.animation:unload()
     self.animation = nil
+end
+
+function character.animation:play()
+    self.animation:play()
+end
+function character.animation:stop()
+    self.animation:stop()
 end
 
 function character.animation:setState(st)
