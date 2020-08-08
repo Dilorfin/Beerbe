9ff50cf28095456c6ff0ba0d499b00088f8c5fef
Dilorfin <pavel.kuybida@gmail.com>
2020-08-08 20:02:51 +0300

    Fix loading objects list

--- scenes/world/objects.lua
+++ scenes/world/objects.lua
@@ -6,8 +6,8 @@ local files = love.filesystem.getDirectoryItems("scenes/world/objects")
 for k, file in ipairs(files) do
     local filename = "scenes/world/objects/"..file
     local obj = love.filesystem.load("scenes/world/objects/"..file)()
-
-    table.insert(objects.filenames, obj.id, filename)
+    
+    objects.filenames[obj.id] = filename
 end
 
 function objects:loadObject(id, x, y)
