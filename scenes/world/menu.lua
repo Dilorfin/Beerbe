a17ea627f12ae634d2b899aa935896823df6322f
Dilorfin <pavel.kuybida@gmail.com>
2020-08-12 20:21:53 +0300

    Add menu for choosing menu

--- scenes/world/menu.lua
+++ scenes/world/menu.lua
@@ -1,18 +1,42 @@
 local menu = {}
 
+character = require "character"
+local chooseMenu = require "scenes/world/menus/choose"
+
+local menuState = {
+    current = 6,
+    items = 1,
+    magic = 2,
+    equipment = 3,
+    donate = 4,
+    settings = 5,
+    chooseMenu = 6
+}
+
 function menu:load()
+    self.font = love.graphics.newFont("asserts/Arial.ttf", love.graphics.getHeight()/24)
+    chooseMenu:load(self.font)
 end
 
 function menu:unload()
+    self.font = nil
+    chooseMenu:unload()
 end
 
 function menu:update(delta_time)
 end
 
 function menu:control_button(command)
+    if menuState.current == menuState.chooseMenu then
+        chooseMenu:control_button(command, menuState)
+    end
+    
 end
 
 function menu:draw()
+    if menuState.current == menuState.chooseMenu then
+        chooseMenu:draw()
+    end
 end
 
 return menu
---
+++ scenes/world/menus/choose.lua
@@ -0,0 +1,49 @@
+local chooseMenu = {
+    index = 1
+}
+
+function chooseMenu:load(font)
+    self.font = font
+    self.options = {
+        love.graphics.newText(self.font, "Items"),
+        love.graphics.newText(self.font, "Magic"),
+        love.graphics.newText(self.font, "Equipment"),
+        love.graphics.newText(self.font, "Donate"),
+        love.graphics.newText(self.font, "Settings"),
+        love.graphics.newText(self.font, "Escape")
+    }
+end
+
+function chooseMenu:unload()
+    self.options = nil
+end
+
+function chooseMenu:control_button(command, menuState)
+    if command == Command.Up then
+        if self.index > 1 then
+            self.index = self.index - 1
+        end
+    elseif command == Command.Down then
+        if self.index < #self.options then
+            self.index = self.index + 1
+        end
+    elseif command == Command.Confirm then
+        if self.index == #self.options then
+            love.event.quit()
+        else
+            menuState.current = self.index
+        end
+    end
+end
+
+function chooseMenu:draw()
+    local offsetX = love.graphics.getWidth()/80
+    local offsetY = love.graphics.getHeight()/2-(#self.options*self.font:getHeight()*1.5)/2
+    for i = 1, #self.options do
+        love.graphics.draw(self.options[i], offsetX,  offsetY + (i-1)*self.font:getHeight()*1.5)
+    end
+    
+    love.graphics.rectangle("fill", offsetX, offsetY + (self.index-1)*self.font:getHeight()*1.5 + self.options[self.index]:getHeight(), self.options[self.index]:getWidth(), 3)
+end
+
+return chooseMenu
