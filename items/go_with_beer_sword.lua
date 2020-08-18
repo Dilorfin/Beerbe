f4e0370eba65dd0a777d5fa92f26357e7801ef19
Dilorfin <pavel.kuybida@gmail.com>
2020-08-18 21:29:03 +0300

    Add loot to dev_chest

---
+++ items/go_with_beer_sword.lua
@@ -0,0 +1,8 @@
+local item = {
+    id = 5,
+    title = "Go with beer sword",
+    type = "sword",
+    damage = 5
+}
+
+return item
--- scenes/world/objects/dev_room/dev_chest.lua
+++ scenes/world/objects/dev_room/dev_chest.lua
@@ -8,22 +8,43 @@ local obj = {
     isOpened = false
 }
 
+local character = require "character"
+local items = require "items"
+
 obj.animation:stop()
 obj.animation:setMode("once")
 
 function obj:onCollide(moving)
     if not self.isOpened then
         moving.info.isShown = true
-        moving.info.text = "Open chest?"
+        moving.info.text = "Open dev chest?"
         moving.info.onConfirm = self.onConfirm
     end
 end
 
-function obj.onConfirm(moving)
+local function getReplicas(itemsToGive)
+    local replicas = {}
+    for i = 1, #itemsToGive do
+        replicas[#replicas+1] = "You got \""..items[itemsToGive[i]].title.."\""
+    end
+    return replicas
+end
 
+function obj.onConfirm(moving)
+    local itemsToGive = { 5, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
+
+    for i = 1, #itemsToGive do
+        character.bag[#character.bag + 1] = itemsToGive[i]
+    end
     
     obj.animation:play()
     obj.isOpened = true
+    local replicas = {
+        "You got \"Go with beer sword\"",
+        "You got \"Beer 1l\" x10"
+    }
+    
+    moving:startDialogue(replicas)
 end
 
 function obj:update(dt)
