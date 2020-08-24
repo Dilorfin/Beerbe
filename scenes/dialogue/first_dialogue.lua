8bee87d2312401a36799b33b2842b6f7ad386ae5
Dilorfin <pavel.kuybida@gmail.com>
2020-08-24 19:01:31 +0300

    Add Act 0

--- scenes/dialogue/first_dialogue.lua
+++ scenes/dialogue/first_dialogue.lua
@@ -1,49 +1,88 @@
 local scenario = {
     resources = {
         backgrounds = {
-            [1] = love.graphics.newImage("asserts/fight/background.png")
+            [1] = love.graphics.newImage("asserts/dialogue/first/frame0.png"),
+            [2] = love.graphics.newImage("asserts/dialogue/first/frame1.png"),
+            [3] = love.graphics.newImage("asserts/dialogue/first/frame2.png"),
+            [4] = love.graphics.newImage("asserts/dialogue/first/frame3.png"),
         },
         animations = {
-            [1] = newAnimation(love.graphics.newImage("asserts/world/objects/dev_room/portal.png"), 48, 48, 0.15, 12)
+            [1] = newAnimation(love.graphics.newImage("asserts/fight/animations/stand.png"), 64, 64, 0.175, 3),
+            [2] = newAnimation(love.graphics.newImage("asserts/dialogue/first/Thunder3.png"), 74, 74, 0.15, 4)
         }
     },
     frames = {
         {
             background = 1,
             title = love.graphics.newText(love.graphics.getFont(), "Disclaimer"),
-            replica = "Текст...",
+            replica = "All characters appearing in this work are fictitious.\nAny resemblance to real persons, living or dead, is purely coincidental.",
+            animations = {}
+        },
+        {
+            background = 2,
+            title = love.graphics.newText(love.graphics.getFont(), "Голос За Кадром"),
+            replica = "Жил был на свете забавный чувачёк, звали его Мишей",
             animations = {
                 {
                     id = 1,
-                    x = 1,
-                    y = 1
+                    x = 500,
+                    y = 400
                 }
             }
         },
         {
-            background = 1,
-            title = love.graphics.newText(love.graphics.getFont(), "???"),
-            replica = "TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText",
+            background = 2,
+            title = love.graphics.newText(love.graphics.getFont(), "Голос За Кадром"),
+            replica = "Жил не тужил, на звезды засматривался",
             animations = {
                 {
                     id = 1,
-                    x = 200,
-                    y = 100
+                    x = 500,
+                    y = 400
                 }
             }
         },
         {
-            background = 1,
-            title = love.graphics.newText(love.graphics.getFont(), "Frame3"),
-            replica = "frame2",
+            background = 3,
+            title = love.graphics.newText(love.graphics.getFont(), "Голос За Кадром"),
+            replica = "в шараге учился",
             animations = {
                 {
                     id = 1,
-                    x = 1,
-                    y = 1
+                    x = 500,
+                    y = 400
                 }
             }
-        }
+        },
+        {
+            background = 4,
+            title = love.graphics.newText(love.graphics.getFont(), "Голос За Кадром"),
+            replica = "да в компьютерные игры играл.",
+            animations = {
+                {
+                    id = 1,
+                    x = 500,
+                    y = 400
+                }
+            }
+        },
+        {
+            background = 4,
+            title = love.graphics.newText(love.graphics.getFont(), "Голос За Кадром"),
+            replica = "и вот однажды...",
+            animations = {
+                {
+                    id = 1,
+                    x = 500,
+                    y = 400
+                },
+                {
+                    id = 2,
+                    x = 495,
+                    y = 395
+                }
+            }
+        },
     }
 }
 
