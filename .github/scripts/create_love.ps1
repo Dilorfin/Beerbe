8625b0fc46520b3529cd4ec8ce6212c8669e278c
Dilorfin <pavel.kuybida@gmail.com>
2021-05-13 18:41:47 +0300

    Update create_love.ps1

--- .github/scripts/create_love.ps1
+++ .github/scripts/create_love.ps1
@@ -1,5 +1,5 @@
 Write-Host "Remove already existig game.love"
-Remove-Item -Path ".build/game.love"
+Remove-Item -Path ".build/game.love" -ErrorAction Ignore
 Write-Host "Create game.zip"
 Get-ChildItem -Path . -Exclude ".*", "love-android" | Compress-Archive -Update -DestinationPath ("./.build/game.zip")
 Write-Host "Renaming game.zip to games.love"
