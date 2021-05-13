e66190418bea7b9e132421a8c35f35a6bd063f67
Dilorfin <pavel.kuybida@gmail.com>
2021-05-13 18:58:30 +0300

    Update create_love.ps1

--- .github/scripts/create_love.ps1
+++ .github/scripts/create_love.ps1
@@ -1,6 +1,7 @@
 Write-Host "Remove already existig game.love"
-Remove-Item -Path ".build\game.love" -ErrorAction Ignore
+Remove-Item -Path ".\.build\game.love" -ErrorAction SilentlyContinue
+New-Item -ItemType Directory -Force -Path ".\.build\" | Out-Null
 Write-Host "Create game.zip"
-Get-ChildItem -Path . -Exclude ".*", "love-android" | Compress-Archive -Update -DestinationPath (".build\game.zip")
+Get-ChildItem -Path "." -Exclude ".*", "love-android" | Compress-Archive -Update -DestinationPath ((Resolve-Path -Path ".\.build\").Path + "game.zip")
 Write-Host "Renaming game.zip to games.love"
-Get-ChildItem ".build\game.zip" | Rename-Item -Force -NewName "game.love"
+Get-ChildItem ".\.build\game.zip" | Rename-Item -Force -NewName "game.love"
