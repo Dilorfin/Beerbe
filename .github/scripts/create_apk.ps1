493581b95fd8677fa622f49f0a7067578c6d9f6f
Dilorfin <pavel.kuybida@gmail.com>
2021-05-13 23:22:37 +0300

    Add windows build scripts

--- .github/scripts/create_apk.ps1
+++ .github/scripts/create_apk.ps1
@@ -6,9 +6,11 @@ Copy-Item ".\.build\game.love" -Destination ".\love-android\app\src\main\assets\
 
 cd ./love-android
 Write-Host "Applying patch"
-git apply "..\.github\scripts\apk-patches\android.patch"
+git apply "..\.github\scripts\apk-data\android.patch"
+
 Write-Host "Unpacking icons"
-Expand-Archive -Path "..\.github\scripts\apk-patches\drawable.zip" -DestinationPath ".\app\src\main\res\" -Force
+Expand-Archive -Path "..\.github\scripts\apk-data\drawable.zip" -DestinationPath ".\app\src\main\res\" -Force
+
 Write-Host "Start build script"
 ./gradlew assembleNormal
 cd ..
